f64a48980051475d13b75eede940092c3c3f6e82
ymatagne <yan.matagne@gmail.com>
2024-06-13 08:58:32 -0400

    init load testing

--- main.py
+++ main.py
@@ -34,9 +34,6 @@ def wait_on_run(run, thread):
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    if not file.filename.endswith('.pdf'):
-        raise HTTPException(status_code=400, detail="Invalid file format")
-
     temp_file_path = f"/tmp/{file.filename}"
     with open(temp_file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -59,7 +56,8 @@ async def upload_file(file: UploadFile = File(...)):
     my_run = client.beta.threads.runs.create(
         thread_id=my_thread.id,
         assistant_id="asst_HNbmFvEPqx8HVJoJ8nD6SE6B",
-        model='gpt-4o-2024-05-13'
+        model='gpt-4o-2024-05-13',
+        temperature=0
     )
     wait_on_run(my_run, my_thread)
 
